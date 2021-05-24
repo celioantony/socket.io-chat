45e12793c2c661696d316b4b29eb70d1f1dc7efa
Celio Soares <celio.soares@itbam.org.br>
2021-05-24 14:30:28 -0400

    add template html to chat

--- index.ts
+++ index.ts
@@ -3,25 +3,33 @@
  */
 import express from 'express';
 import http from 'http';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
+import ApplicationConfig from './ApplicationConfig';
 
 const app = express();
 const io = new Server(new http.Server(app), {});
 
-// Load config middleware
-// config(app);
+export default class Application {
+  constructor() {
+    // Load config middleware
+    let config = new ApplicationConfig(app);
+    config.init();
 
-// Define Socket.IO
-io.on('connection', (socket) => {
-  console.log('User connected!');
-});
+    // Define Socket.IO
+    io.on('connection', (socket) => {
+      console.log('User connected!');
+    });
 
-// Define server port
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log('=======================');
-  console.log('... Server started ...');
-  console.log(`Running in port: ${PORT}`);
-  console.log('Application: Socket.IO Chat')
-  console.log('=======================');
-});
+    // Define server port
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log('=======================');
+      console.log('... Server started ...');
+      console.log(`Running in port: ${PORT}`);
+      console.log('Application: Socket.IO Chat')
+      console.log('=======================');
+    });
+  }
+}
+
+module.exports = new Application();
